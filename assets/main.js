c0fc64025d0fc1c3a9c64fe850d8a0419e724a06
Maruf7219 <inamdarmaruf13@gmail.com>
2025-07-16 17:20:43 +0530

    our work section completed

--- assets/main.js
+++ assets/main.js
@@ -1,41 +1,47 @@
-    const swiper = new Swiper('.mySwiper', {
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 18,
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
       slidesPerView: 3,
-      spaceBetween: 24,
-      loop: true,
-      breakpoints: {
-        0: {
-          slidesPerView: 1
-        },
-        768: {
-          slidesPerView: 2
-        },
-        992: {
-          slidesPerView: 3
-        }
-      }
-    });
-    // Custom buttons
-    document.getElementById("prevSlide").addEventListener("click", () => swiper.slidePrev());
-    document.getElementById("nextSlide").addEventListener("click", () => swiper.slideNext());
+    },
+  },
+});
+// Custom buttons
+document
+  .getElementById("prevSlide")
+  .addEventListener("click", () => swiper.slidePrev());
+document
+  .getElementById("nextSlide")
+  .addEventListener("click", () => swiper.slideNext());
 
-    const ourWorkSwiper = new Swiper('.our-work-swiper', {
+const ourWorkSwiper = new Swiper(".our-work-swiper", {
+  slidesPerView: 3,
+  spaceBetween: 16,
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
       slidesPerView: 3,
-      spaceBetween:16,
-      loop: true,
-      breakpoints: {
-        0: {
-          slidesPerView: 1
-        },
-        768: {
-          slidesPerView: 2
-        },
-        992: {
-          slidesPerView: 3
-        }
-      }
-    });
-    // Custom buttons
-    document.querySelector(".our-work-left-btn").addEventListener("click", () => swiper.slidePrev());
-    document.querySelector(".our-work-right-btn").addEventListener("click", () => swiper.slideNext());
-
-    
+    },
+  },
+});
+// Custom buttons
+document
+  .querySelector(".our-work-left-btn")
+  .addEventListener("click", () => ourWorkSwiper.slidePrev());
+document
+  .querySelector(".our-work-right-btn")
+  .addEventListener("click", () => ourWorkSwiper.slideNext());
