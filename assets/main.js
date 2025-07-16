ad20553ec185423fd6eea2602dc52a3c60a1c8e3
Maruf7219 <inamdarmaruf13@gmail.com>
2025-07-16 15:50:55 +0530

    updates

--- assets/main.js
+++ assets/main.js
@@ -17,3 +17,25 @@
     // Custom buttons
     document.getElementById("prevSlide").addEventListener("click", () => swiper.slidePrev());
     document.getElementById("nextSlide").addEventListener("click", () => swiper.slideNext());
+
+    const ourWorkSwiper = new Swiper('.our-work-swiper', {
+      slidesPerView: 3,
+      spaceBetween:16,
+      loop: true,
+      breakpoints: {
+        0: {
+          slidesPerView: 1
+        },
+        768: {
+          slidesPerView: 2
+        },
+        992: {
+          slidesPerView: 3
+        }
+      }
+    });
+    // Custom buttons
+    document.querySelector(".our-work-left-btn").addEventListener("click", () => swiper.slidePrev());
+    document.querySelector(".our-work-right-btn").addEventListener("click", () => swiper.slideNext());
+
+    
