e13abd7ef000a2910e2ef95f8fe958592d79d5a5
Gorakh Kharat <gorakhkharat@Gorakhs-MacBook-Pro.local>
2025-07-30 16:03:16 +0530

    condtional statemenrt js

---
+++ day-2.js
@@ -0,0 +1,63 @@
+// var a = 10;
+// if( a >11 ) {
+//     console.log("a is greater than 5");
+// }else {
+//     console.log("a is less than or equal to 5");
+// }
+
+// var age = prompt("Enter your age");
+// if (age >= 18) {
+//     console.log("You are eligible to vote");
+// } else {
+//     console.log("You are not eligible to vote");
+// }
+
+// let marks = 55;
+// // debugger
+// if (marks >= 90) {
+//     console.log("Grade A");
+// }else if (marks >= 80) {
+//     console.log("Grade B");
+// }else if (marks >= 70) {
+//     console.log("Grade C")
+// } else {
+//     console.log("Enetr number between 70 to 100");
+// }
+// var age = 20;
+// var hasVoterID = false;
+// if(age >= 18){
+//     if(hasVoterID) {
+//         console.log("You can vote");
+//     } else {
+//         console.log("You need a voter ID to vote");
+//     }
+// }
+// var age = 20;
+// age >= 18 ? console.log("You can vote") : console.log("You cannot vote");
+
+// var day = 8;
+
+// switch(day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thu");
+//         break;
+//     case 5:
+//         console.log("Fri");
+//         break;
+//     case 6:
+//         console.log("Sat");
+//         break;  
+
+//         default:
+//         console.log("Sunday");
+//         break;
+// }
